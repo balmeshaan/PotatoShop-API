9749aa6508ec1e0bcc4b02bcf77a4d5ea565dc5f
barak <balmeshaan@gmail.com>
2020-05-10 07:28:56 +0300

    add route for detail pages

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ app.get("/potatoes", (req, res) => {
   res.json(data);
 });
 
+app.get("/potatoes/:potatoId", (req, res) => {
+  const { potatoId } = req.params;
+  const potatoFound = data.find((potato) => potato.id === +potatoId);
+  if (potatoFound) {
+    res.json(potatoFound);
+  } else {
+    res.status(404).json({ message: "Potato not found" });
+  }
+});
+
 app.listen(8000, () => {
   console.log("This app is running on localhost:8000");
 });
